16b5dbfde3437dd26302f3373733d63a11548fb9
jeeweb <jee.vvvv@gmail.com>
2022-08-10 14:52:35 +0900

    회원가입post, nodemon설치

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://jiee:abcd1234@atlascluster.1dgjpcz.mongodb.net/?retryWrites=true&w=majority', {
@@ -9,8 +17,20 @@ mongoose.connect('mongodb+srv://jiee:abcd1234@atlascluster.1dgjpcz.mongodb.net/?
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello!')
 })
+
+app.post('/register', (req, res) => {
+  // 회원가입에 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어줌
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  });
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
