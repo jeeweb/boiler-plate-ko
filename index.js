3c3f4978e217265592d1ac9e9e1949445bd14a06
jeeweb <jee.vvvv@gmail.com>
2022-08-10 14:59:31 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 5000;
 const bodyParser = require('body-parser');
 const { User } = require('./models/User');
 
+const config = require('./config/key');
+
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -11,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jiee:abcd1234@atlascluster.1dgjpcz.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
