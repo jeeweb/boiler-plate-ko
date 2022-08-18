7e6b5f74794604d8107706cafee3e84f39489241
jeeweb <jee.vvvv@gmail.com>
2022-08-18 17:33:44 +0900

    로그아웃기능

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,25 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      //console.log(response.data);
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃에 실패했습니다.");
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -16,6 +31,7 @@ function LandingPage() {
       }}
     >
       <h2>시작페이지</h2>
+      <button onClick={onClickHandler}>Logout</button>
     </div>
   );
 }
